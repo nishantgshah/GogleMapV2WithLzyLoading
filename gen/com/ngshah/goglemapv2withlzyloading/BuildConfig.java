c0dba0415b6c713232f44c50636ba531ae46246c
nishantgshah <nishant.g.shah@gmail.com>
2013-07-31 17:39:13 +0530

    Initial

---
+++ gen/com/ngshah/goglemapv2withlzyloading/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.ngshah.goglemapv2withlzyloading;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
